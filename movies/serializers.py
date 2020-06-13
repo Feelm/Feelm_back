fcd0e7306376a6cd044ef282ba9052b3b1d7b54b
hojin <redsmile123@naver.com>
2020-06-13 11:27:02 +0900

    movielist까지

---
+++ movies/serializers.py
@@ -0,0 +1,27 @@
+from rest_framework import serializers
+from .models import Movie, Movie_Star_Point
+from django.contrib.auth import get_user_model
+
+
+User = get_user_model()
+# class ArticleListSerializer(serializers.ModelSerializer):
+#     # user = UserSerializer()
+
+#     class Meta:
+#         model = Article
+#         fields = ('id', 'title', 'user',)
+
+class MovieStarPointSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Movie_Star_Point
+        fields = ('user', 'movie', 'star_point')
+
+
+class MovieListSerializer(serializers.ModelSerializer):
+    pointing_users = MovieStarPointSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Movie
+        fields = ('id', 'title', 'pointing_users',)
+
--- movies/views.py
+++ movies/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+
+from rest_framework.response import Response
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+
+from .models import Movie, Movie_Star_Point
+from .serializers import MovieListSerializer
+
+# 라인브레이크
+# from django.template.defaultfilters import linebreaks
 
 # Create your views here.
+@api_view(['GET'])
+def index(request):
+    movies = Movie.objects.order_by('-release_date')
+    serializer = MovieListSerializer(movies, many=True)
+    return Response(serializer.data)
