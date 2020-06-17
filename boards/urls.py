646bb7d83b3ad9bf8234bfcf4cef820188653511
hojin <redsmile123@naver.com>
2020-06-17 18:34:52 +0900

    장고 마무리

--- boards/urls.py
+++ boards/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path('requests/', views.requests, name='requests'),
     path('free/' , views.free, name='free'),
     path('free/<int:free_pk>/', views.free_detail, name='free_detail'),
-    path('free/<int:free_pk>/like', views.free_like, name='free_like'),
+    path('free/<int:free_pk>/like/', views.free_like, name='free_like'),
     
 ]
