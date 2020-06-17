1c365ef0a38367adc310457ffd7a0012e63c6e34
hojin <redsmile123@naver.com>
2020-06-17 21:36:23 +0900

    장고 내쪽 마무리

--- accounts/views.py
+++ accounts/views.py
@@ -2,9 +2,11 @@ from django.shortcuts import get_object_or_404
 from django.contrib.auth import get_user_model
 from rest_framework.response import Response
 from rest_auth.models import TokenModel
+from rest_framework.decorators import api_view, permission_classes
 # Create your views here.
 
 User=get_user_model()
+@api_view(['GET'])
 def withdraw(request, user_pk):
     user= get_object_or_404(User,pk=user_pk)
     if request.user == user:
